b7a6aa662c52d317fd055e6d447751ce56183b3f
abhishah <abhishah0961@gmail.com>
2022-08-01 18:02:42 +0530

    [Add] auth module and auth service

---
+++ src/auth/auth.module.ts
@@ -0,0 +1,20 @@
+import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { JwtModule } from '@nestjs/jwt';
+import { AuthService } from './services/auth.service';
+
+@Module({
+    imports: [
+        JwtModule.registerAsync({
+            imports: [ConfigModule],
+            inject: [ConfigService],
+            useFactory: async () => ({
+                secret: process.env.JWT_SECRET,
+                signOptions: { expiresIn: '10000s' }
+            })
+        })
+    ],
+    providers: [AuthService],
+    exports:[AuthService]
+})
+export class AuthModule { }
