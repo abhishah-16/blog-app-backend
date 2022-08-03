bf018089847345a9251f62fa38b266ba02225ef4
abhishah <abhishah0961@gmail.com>
2022-08-03 14:35:14 +0530

    [added] create blog endpoint

--- src/blog/controller/blog.controller.ts
+++ src/blog/controller/blog.controller.ts
@@ -1,4 +1,19 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post, UseGuards, Request } from '@nestjs/common';
+import { Observable } from 'rxjs';
+import { jwtAuthGuard } from 'src/auth/guards/jwt.guard';
+import { Blog } from '../models/blog.interface';
+import { BlogService } from '../services/blog.service';
 
-@Controller('blog')
-export class BlogController {}
+@Controller('blogs')
+export class BlogController {
+    constructor(
+        private blogservice: BlogService
+    ) { }
+
+    @UseGuards(jwtAuthGuard)
+    @Post()
+    create(@Body() blog: Blog, @Request() req): Observable<Blog> {
+        const user = req.user.user
+        return this.blogservice.create(user, blog)
+    }
+}
--- src/blog/services/blog.service.ts
+++ src/blog/services/blog.service.ts
@@ -1,4 +1,35 @@
 import { Injectable } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { from, Observable, of, switchMap } from 'rxjs';
+import { AuthService } from 'src/auth/services/auth.service';
+import { User } from 'src/user/model/user.interface';
+import { Repository } from 'typeorm';
+import { BlogEntryEntity } from '../models/blog.entity';
+import { Blog } from '../models/blog.interface';
+import { UserService } from '../../user/services/user.service'
+import slugify from 'slugify';
 
 @Injectable()
-export class BlogService {}
+export class BlogService {
+
+    constructor(
+        @InjectRepository(BlogEntryEntity) private readonly blogRepository: Repository<BlogEntryEntity>,
+        private authservice: AuthService,
+        private userservice: UserService
+    ) { }
+
+
+    create(user: User, blog: Blog): Observable<Blog> {
+        blog.author = user
+        return this.generateSlug(blog.title).pipe(
+            switchMap((slug: string) => {
+                blog.slug = slug
+                return from(this.blogRepository.save(blog))
+            })
+        )
+    }
+
+    generateSlug(title: string): Observable<string> {
+        return of(slugify(title))
+    }
+}
